dcc0746b81220ee3a53b1744ab62ced9eb5d2ca7
Joao Paulo <59973999+jon-coder@users.noreply.github.com>
2022-05-11 22:24:07 -0300

    finish lesson 12

---
+++ atividade_12/app.js
@@ -0,0 +1,12 @@
+function closedWindow(obj) {
+    console.log('PASSEI AQUI')
+    obj.style.backgroundImage = "url(assets/closed.png)";
+}
+
+function openWindow(obj) {
+    obj.style.backgroundImage = "url(assets/open.png)";
+}
+
+function brokenWindow(obj) {
+    obj.style.backgroundImage = "url(assets/broken.jpg)";
+}
