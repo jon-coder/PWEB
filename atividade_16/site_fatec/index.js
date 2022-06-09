303d4b71782918d14c3f44ebf47170c671a66af4
João Paulo Camargo <joaopaulobrasiliano@gmail.com>
2022-06-09 09:07:12 -0300

    refactor: file structure adjustment for lesson 16

--- atividade_16/site_fatec/index.js
+++ atividade_16/site_fatec/index.js
@@ -1,7 +1,7 @@
 var text = require('./modulo1');
 var app = require('./app/config/server')
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('Server with express has been load!');
     console.log(text)
 });
